106ae6a5c0083ff7b0c5f5b3fcf4898cc2f59bd0
Franco Cipollone <franco.c@ekumenlabs.com>
2023-04-13 12:11:20 -0300

    Rebase from 'release/foxy/maliput_dragway'

--- include/maliput_dragway/params.h
+++ include/maliput_dragway/params.h
@@ -30,41 +30,55 @@
 // Copyright 2021 Toyota Research Institute
 #pragma once
 
-#include <memory>
-
-#include <maliput/api/road_geometry.h>
-#include <maliput/api/road_network.h>
-
 namespace maliput {
 namespace dragway {
+namespace params {
 
-/// Contains the attributes needed for building a dragway::RoadGeometry.
-struct RoadGeometryConfiguration {
-  /// Create a RoadGeometryConfiguration from a string dictionary.
-  static maliput::dragway::RoadGeometryConfiguration FromMap(const std::map<std::string, std::string>& parameters);
+/// @defgroup road_geometry_configuration_keys RoadGeometry configuration builder keys
+///
+/// Parameters used during the RoadGeometry building process.
+///
+/// When parameters are omitted the default value will be used.
+///
+/// Example of use:
+/// @code{cpp}
+/// #include <maliput_dragway/params.h>
+/// #include <maliput_dragway/road_network_builder.h>
+/// // ...
+/// const std::map<std::string, std::string> builder_configuration {
+///   {maliput::dragway::params::kNumLanes, "4"},
+///   {maliput::dragway::params::kLength, "150"},
+///   {maliput::dragway::params::kLaneWidth, "3"},
+/// };
+/// auto road_network = maliput::dragway::BuildRoadNetwork(builder_configuration)();
+/// @endcode
+///
+/// @{
 
-  /// @details The keys of the map are listed at @ref road_geometry_configuration_keys.
-  /// @returns A string-string map containing the road geometry configuration.
-  std::map<std::string, std::string> ToStringMap() const;
+/// Number of lanes.
+///   - Default: @e "2"
+static constexpr char const* kNumLanes{"num_lanes"};
+/// Length of the dragway.
+///   - Default: @e "10"
+static constexpr char const* kLength{"length"};
+/// Width of the lanes.
+///   - Default: @e "3.7"
+static constexpr char const* kLaneWidth{"lane_width"};
+/// Width of the shoulders of the road.
+///   - Default: @e "3."
+static constexpr char const* kShoulderWidth{"shoulder_width"};
+/// Maximum height above the road surface.
+///   - Default: @e "5.2"
+static constexpr char const* kMaximumHeight{"maximum_height"};
+/// Translation from maliput to maliput_osm inertial frame.
+/// The format of the 3-dimensional vector that is expected to be passed
+/// should be {X, Y, Z}. Same format as maliput::math::Vector3 is
+/// serialized.
+///   - Default: @e "{0., 0., 0.}"
+static constexpr char const* kInertialToBackendFrameTranslation{"inertial_to_backend_frame_translation"};
 
-  /// Number of lanes.
-  int num_lanes{2};
-  /// Length of the lanes.
-  double length{10};
-  /// Width of the lanes.
-  double lane_width{3.7};
-  /// Width of the shoulders of the road.
-  double shoulder_width{3.};
-  /// Maximum height above the road surface.
-  double maximum_height{5.2};
-  /// Inertial to Backend Frame translation.
-  math::Vector3 inertial_to_backend_frame_translation{0., 0., 0.};
-};
+/// @}
 
-/// Builds an api::RoadNetwork based on Dragway implementation.
-/// @param road_geometry_configuration Holds the properties to build the RoadNetwork.
-/// @return A maliput::api::RoadNetwork.
-std::unique_ptr<api::RoadNetwork> BuildRoadNetwork(const RoadGeometryConfiguration& road_geometry_configuration);
-
+}  // namespace params
 }  // namespace dragway
 }  // namespace maliput
